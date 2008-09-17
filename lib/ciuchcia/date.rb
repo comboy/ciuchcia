2b99fb254b4a86591ec44a24457d3bb6da9834b2
comboy <kacper.ciesla@gmail.com>
2008-09-17 23:39:07 +0200

    name days updated
    second smaller, and probably better name days list
    working at Date module

--- lib/ciuchcia/date.rb
+++ lib/ciuchcia/date.rb
@@ -1,12 +1,21 @@
-if Object.const_defined? 'Date' and Ciuchcia.const_defined? 'PATCH_DATE'
-
-  class CiuchciaDate < Date
+module Ciuchcia 
+  class Date
     MONTHNAMES = [nil] + %w{Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień}
+    ABBR_MONTHNAMES = [nil] + %w{Sty Lut Mar Kwi Maj Cze Lip Sie Wrz Paź Lis Gru}
     DAYNAMES = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"]
+    ABBR_DAYNAMES = ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"]
   end
+end
 
-  # This generates warning, that's definitely bad
-  # If you know how to avoid it, please let me know
-  Date = CiuchciaDate
+class Time
+  alias :strftime_nolocale :strftime
 
+  def strftime(format)
+    format = format.dup
+    format.gsub!(/%a/, Ciuchcia::Date::ABBR_DAYNAMES[self.wday])
+    format.gsub!(/%A/, Ciuchcia::Date::DAYNAMES[self.wday])
+    format.gsub!(/%b/, Ciuchcia::Date::ABBR_MONTHNAMES[self.mon])
+    format.gsub!(/%B/, Ciuchcia::Date::MONTHNAMES[self.mon])
+    self.strftime_nolocale(format)
+  end
 end
--- lib/ciuchcia/name_days.rb
+++ lib/ciuchcia/name_days.rb
@@ -11,6 +11,16 @@ module Ciuchcia
       t = Time.now
       list[t.month][t.day]
     end
+    
+    def self.list_full
+      @@name_days = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)),'..','..','config','name_days_full.yml' ))
+    end    
+
+    def self.today_full
+      t = Time.now
+      list_full[t.month][t.day]
+    end
+
   end
    
 end
